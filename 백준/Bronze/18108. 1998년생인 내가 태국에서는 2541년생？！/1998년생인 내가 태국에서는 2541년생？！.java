c0c074d8574861161d78e7ead4c2419d7c4d4b40
Heeju Lee <heejulia21@naver.com>
2024-12-19 14:36:09 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = 543;
+        int y = sc.nextInt();
+
+        System.out.println(y-x);
+    }
+}
