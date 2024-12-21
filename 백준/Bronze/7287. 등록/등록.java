30b5c4d527e0252124648528d883d4e2a21914ac
Heeju Lee <heejulia21@naver.com>
2024-12-21 23:22:06 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(34);
+        System.out.println("heejulia21");
+    }
+}
