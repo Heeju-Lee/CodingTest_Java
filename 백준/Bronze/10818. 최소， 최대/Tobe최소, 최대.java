925bc99e5bf966e1c1301f2eafcd3b9b643fff26
Heeju Lee <heejulia21@naver.com>
2024-12-22 16:08:10 +0900

    Create Tobe최소, 최대.java
    - Stream 사용 시, n번 순회함.
    - 간단한 최소, 최대 값 구하기는 list 보다는 배열사용
        - 메모리 최적화

---
+++ 백준/Bronze/10818. 최소， 최대/Tobe최소, 최대.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+
+        System.out.println(min+" "+max);
+        br.close();
+    }
+}
