b1f722fc211960125464fa2b4ee524bd042317a6
Heeju Lee <heejulia21@naver.com>
2024-12-23 22:18:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String n = br.readLine();
+        char ch = n.charAt(0);
+
+        System.out.println((int)ch);
+    }
+}
