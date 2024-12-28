c62ba9a6900220cadb200ea9a19e637dd7ce3324
Heeju Lee <heejulia21@naver.com>
2024-12-28 23:10:43 +0900

    [Bronze II] Title: OX퀴즈, Time: 124 ms, Memory: 16036 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -10,21 +10,19 @@ public class Main {
 
         for(int i = 0; i < n; i++){
             String input = br.readLine();
-            String before = "";
             String now;
             int score = 0;
             int totalScore = 0;
 
             for(int j = 0; j < input.length(); j++){
                 now = String.valueOf(input.charAt(j));
-                    if(now.equals("O")){
-                        score++;
-                    }
-                    else{
-                        score = 0;
-                    }
-                    totalScore += score;
-                    before = now;
+                if(now.equals("O")){
+                    score++;
+                }
+                else{
+                    score = 0;
+                }
+                totalScore += score;
             }
             bw.write(totalScore+"\n");
         }
